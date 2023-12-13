8ad89f1091867f1885648f6532dd09e7fa34097f
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-12-13 13:04:42 +0100

    commit

--- protocol/protocol.py
+++ protocol/protocol.py
@@ -1,16 +1,26 @@
 import struct
 
-class Protocoll():
+class Protocol:
     def __init__(self):
         pass
 
+    @staticmethod
     def encode_tlv(type, value):
         length = len(value)
         tlv_format = "!B H {}s".format(length)
-        tlv_data = struct.pack(tlv_format, type, length, value)
+        tlv_data = struct.pack(tlv_format, type, length, value.encode())
         return tlv_data
 
+    @staticmethod
     def decode_tlv(data):
         type_length = struct.unpack("!B H", data[:3])
         type_value = data[3:]
         return type_length[0], type_length[1], type_value.decode()
+
+encoded_data = Protocol.encode_tlv(1, "Hello")
+print(f"Encoded Data: {encoded_data}")
+
+decoded_type, decoded_length, decoded_value = Protocol.decode_tlv(encoded_data)
+print(f"Decoded Type: {decoded_type}")
+print(f"Decoded Length: {decoded_length}")
+print(f"Decoded Value: {decoded_value}")
