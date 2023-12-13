7047acb0ede7c1aa88a85b5e964e4b1efba89680
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-12-13 13:07:16 +0100

    lol

--- protocol/main.py
+++ protocol/main.py
@@ -4,4 +4,12 @@ from protocol import Protocol
 def main():
     Protocol()
 
+    encoded_data = Protocol.encode_tlv(1, "Hello")
+    print(f"Encoded Data: {encoded_data}")
+
+    decoded_type, decoded_length, decoded_value = Protocol.decode_tlv(encoded_data)
+    print(f"Decoded Type: {decoded_type}")
+    print(f"Decoded Length: {decoded_length}")
+    print(f"Decoded Value: {decoded_value}")
+
 main()
--- protocol/protocol.py
+++ protocol/protocol.py
@@ -1,6 +1,6 @@
 import struct
 
-class Protocoll():
+class Protocol():
     def __init__(self):
         print("protocol constructor")
 
@@ -16,11 +16,3 @@ class Protocoll():
         type_length = struct.unpack("!B H", data[:3])
         type_value = data[3:]
         return type_length[0], type_length[1], type_value.decode()
-
-encoded_data = Protocol.encode_tlv(1, "Hello")
-print(f"Encoded Data: {encoded_data}")
-
-decoded_type, decoded_length, decoded_value = Protocol.decode_tlv(encoded_data)
-print(f"Decoded Type: {decoded_type}")
-print(f"Decoded Length: {decoded_length}")
-print(f"Decoded Value: {decoded_value}")
