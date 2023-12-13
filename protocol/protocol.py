ddeb2e2594e8decb909733e2727491bdcb5083cf
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-12-13 12:50:53 +0100

    blap

--- protocol/protocol.py
+++ protocol/protocol.py
@@ -3,6 +3,7 @@ import struct
 class Protocoll():
     def __init__(self):
         pass
+
     def encode_tlv(type, value):
         length = len(value)
         tlv_format = "!B H {}s".format(length)
